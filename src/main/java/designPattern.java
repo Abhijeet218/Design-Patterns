cb6c923fd36c4259707a5ba852baa687f6ec97fe
Abhijeet218 <abhijeet21895@gmail.com>
2022-01-09 16:48:39 +0530

    Singleton class example with break feature

--- src/main/java/designPattern.java
+++ src/main/java/designPattern.java
@@ -1,13 +1,33 @@
-import java.util.Arrays;
+import Singleton.Singleton;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class designPattern {
     public static void main(String arg[]){
 
-        //Singleton
-        Singleton s1=Singleton.getInstance();
+        //Singleton.Singleton
+        Singleton s1= Singleton.getInstance();
         Singleton s2=Singleton.getInstance();
+        Singleton s3break=null;
+        //Breaking Singleton.Singleton Pattern
+        Constructor[] constructors=Singleton.class.getDeclaredConstructors();
+        for(Constructor constructor:constructors){
+            constructor.setAccessible(true);
+            try {
+                s3break= (Singleton) constructor.newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+
         System.out.println("S1 : "+s1);
         System.out.println("S2 : "+s2);
+        System.out.println("S3 : "+s3break);
         System.out.println(s1.equals(s2));
     }
 }
